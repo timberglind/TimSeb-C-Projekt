9f149f90af5fad1513614db51d58495cf2033cf3
tim <berglind.tim@gmail.com>
2017-10-18 11:05:51 +0200

    eqw

--- WindowsFormsApp1/WindowsFormsApp2/Program.cs
+++ WindowsFormsApp1/WindowsFormsApp2/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1;
 
 namespace WindowsFormsApp2
 {
@@ -16,7 +17,7 @@ namespace WindowsFormsApp2
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Window());
         }
     }
 }
